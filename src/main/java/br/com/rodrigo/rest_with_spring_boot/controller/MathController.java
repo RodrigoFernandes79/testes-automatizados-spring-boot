c925a27f9dcbd7ecf53ca8680ab08ba192ece46b
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-01-14 19:25:08 -0300

    Definindo a logica da aplicação

--- src/main/java/br/com/rodrigo/rest_with_spring_boot/controller/MathController.java
+++ src/main/java/br/com/rodrigo/rest_with_spring_boot/controller/MathController.java
@@ -8,8 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
     @GetMapping("/soma/{primeiroNumero}/{segundoNumero}")
-    public Double soma(@PathVariable String primeiroNumero, @PathVariable String segundoNumero ) {
+    public Double soma(@PathVariable String primeiroNumero, @PathVariable String segundoNumero) {
+        if (!isNumerico(primeiroNumero) || !isNumerico(segundoNumero)) {
+            throw new RuntimeException();
+        }
+        return converterParaDouble(primeiroNumero) + converterParaDouble(segundoNumero);
+    }
 
-        return 1D;
+    private Double converterParaDouble(String numeroString) {
+        if (numeroString == null) {
+            return 0D;
+        }
+        // ao digitar 4,5 ele substitui por 4.5
+        String numero = numeroString.replace(",", ".");
+        return Double.parseDouble(numero);
+    }
+
+    private boolean isNumerico(String numeroString) {
+        if (numeroString == null) {
+            return false;
+        }
+        String numero = numeroString.replaceAll(",", ".");
+        //verifica se ele bate com o valor numerico (o regex abaixo verifica se o valor é numerico)
+        return numero.matches("^-?\\d+(\\.\\d+)?$");
     }
 }
