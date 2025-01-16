d5744244f28fac936309b1f3ccfdfa584159b4a5
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-01-16 08:26:32 -0300

    ajustando o nome da inteface PessoaRepository

--- src/main/java/br/com/rodrigo/rest_with_spring_boot/repository/PessoaRepository.java
+++ src/main/java/br/com/rodrigo/rest_with_spring_boot/repository/PessoaRepository.java
@@ -3,5 +3,5 @@ package br.com.rodrigo.rest_with_spring_boot.repository;
 import br.com.rodrigo.rest_with_spring_boot.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface pessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 }
