e025f832e9e31eb3919d9233a1a4e5ae5eae8d9b
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-01-18 14:23:29 -0300

    Testando classe repository - metodos put, get, post delete

--- src/main/java/br/com/rodrigo/rest_with_spring_boot/repository/PessoaRepository.java
+++ src/main/java/br/com/rodrigo/rest_with_spring_boot/repository/PessoaRepository.java
@@ -3,5 +3,8 @@ package br.com.rodrigo.rest_with_spring_boot.repository;
 import br.com.rodrigo.rest_with_spring_boot.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    Optional<Pessoa> findByEmail(String email);
 }
